9e6e3e02f53d5fe0c5d0039d5293f68327c8c55b
Saman-naruee <samannaruee@gmail.com>
2024-08-08 14:11:51 +0330

    add serialzers.py and update urls with router and customize admin panel.

--- students/admin.py
+++ students/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Student
 
-# Register your models here.
+# admin.site.register(Student)
+
+@admin.register(Student)
+class StudentAdmin(admin.ModelAdmin):
+    list_display = ('first_name', 'grade', 'last_name')
+    search_fields = ('first_name',)
+    
+
---
+++ students/urls.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import StudentViewSet
+router = DefaultRouter()
+router.register(r'students', StudentViewSet)
+urlpatterns = [
+    path('', include(router.urls))
+]
