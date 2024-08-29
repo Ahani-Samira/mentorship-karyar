f3a4cfc618505b0975067be779be96aecd443a34
Ahani-Samira <140585948+Ahani-Samira@users.noreply.github.com>
2024-08-29 13:28:50 +0330

    Add "create_student" view with api_view decorator and corresponding URL

--- students/urls.py
+++ students/urls.py
@@ -1,9 +1,15 @@
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import StudentViewSet
+from .views import StudentViewSet, create_student
+
+
 router = DefaultRouter()
 router.register(r'students', StudentViewSet)
 urlpatterns = [
-    path('', include(router.urls))
+    path('', include(router.urls)),
+    path('create', create_student, name='create_student'),
 ]
+
+# data = '{"first_name": "Asma","last_name": "Ghahramani", "age": "25", "grade":"20"}'
+# curl -X POST http://127.0.0.1:8000/api/students/create -H "Content-Type: application/json" -d '{"first_name": "Asma","last_name": "Ghahramani", "age": 25, "grade":20}'
--- students/views.py
+++ students/views.py
@@ -1,9 +1,25 @@
+from django.core.serializers import serialize
 from django.shortcuts import render
-from rest_framework import viewsets
+from rest_framework import viewsets, status
+from rest_framework.decorators import api_view
+from django.http import JsonResponse
+from rest_framework.parsers import JSONParser
+from rest_framework.response import Response
+
 from .models import Student
 from .serializers import StudentSreializer
 
+
 class StudentViewSet(viewsets.ModelViewSet):
     queryset = Student.objects.all()
-
     serializer_class = StudentSreializer
+
+@api_view(['POST'])
+def create_student(request):
+    if request.method == 'POST':
+        data = JSONParser().parse(request)
+        serializer = StudentSreializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(data=serializer.data, status=status.HTTP_201_CREATED)
+        return JsonResponse(data=serializer.errors, status=status.HTTP_400_BAD_REQUEST)
